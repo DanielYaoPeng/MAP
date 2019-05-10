c67d62adf8e6822c964744fb45e8e1f71b4d489e
yaopeng <down953@163.com>
2019-05-10 13:32:56 +0800

    BaseServides

--- MD.Apk.MAP.Services/AdvertisementServices.cs
+++ MD.Apk.MAP.Services/AdvertisementServices.cs
@@ -1,6 +1,8 @@
 ﻿using MD.ApkMAP.IRepository;
 using MD.ApkMAP.IServices;
+using MD.ApkMAP.Model.DBModels;
 using MD.ApkMAP.Repository;
+using MD.ApkMAP.Services.Base;
 using System;
 
 namespace MD.ApkMAP.Services
@@ -8,7 +10,7 @@ namespace MD.ApkMAP.Services
     /// <summary>
     /// 业务层，处理具体业务逻辑
     /// </summary>
-    public class AdvertisementServices : IAdvertisementServices
+    public class AdvertisementServices : BaseServices<AdvertTest>, IAdvertisementServices
     {
         IAdvertisementRepository _dal;
 
@@ -23,5 +25,7 @@ namespace MD.ApkMAP.Services
             return _dal.Sum(i, j);
 
         }
+
+
     }
 }
