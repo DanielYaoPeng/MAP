e4d974b0e44791533286d8be570404c8a1573b6e
DanielYaoPeng <down953@163.com>
2019-11-09 14:09:47 +0800

    无效文件注释删除

--- MD.ApkMAP/Startup.cs
+++ MD.ApkMAP/Startup.cs
@@ -1,36 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using Autofac;
-using Autofac.Extensions.DependencyInjection;
-using MD.ApkMAP.AOP;
-using MD.ApkMAP.AuthHelper.OverWrite;
 using MD.ApkMAP.AuthHelper.Policys;
 using MD.ApkMAP.Common.GlobalVar;
-using MD.ApkMAP.IRepository;
-using MD.ApkMAP.IServices;
-using MD.ApkMAP.Repository;
-using MD.ApkMAP.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.Filters;
-using Swashbuckle.AspNetCore.Swagger;
 
 namespace MD.ApkMAP
 {
